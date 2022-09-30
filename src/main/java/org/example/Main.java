e0b6549f8f65aa6f6e873d2ca5bfe3b22511a153
notjuary <barlottifrancesco@gmail.com>
2022-09-30 18:28:08 +0200

    aggiunto il supporto all'italiano

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("0.Inglese \n1.Italiano");
+
+        int s = in.nextInt();
+
+        if (s == 1)
+            System.out.println("Ciao Mondo !");
+        else
+            System.out.println("Hello World !");
+
     }
 }
